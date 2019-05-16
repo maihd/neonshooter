e8dd3d3309471bd828e243b0ea01451986288fe4
MaiHD <maihd.dev@gmail.com>
2019-05-16 23:57:03 +0700

    Update

---
+++ ThirdParty/Mojo/MojoScript/MojoScript.lua
@@ -0,0 +1,1 @@
+return require "Sources.Loader"
---
+++ ThirdParty/Mojo/MojoScript/temp.lua
@@ -0,0 +1,10 @@
+
+
+
+
+
+
+local user1 = {firstname = "Lewis", middlename = "Allan", lastname = "Reed"}
+local user2 = {lastname = "Reed", firstname = "Lou"}
+
+
