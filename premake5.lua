d8060aaac84b06d67e41b1c019766ba4f0712e32
maihd <maihd.dev@gmail.com>
2022-10-06 17:21:49 +0700

    add build system and mailib

--- premake5.lua
+++ premake5.lua
@@ -1,35 +1,34 @@
-local PROJ_DIR = path.getabsolute(".")
-local PROJ_BUILD_DIR = path.join(PROJ_DIR, "Build")
+local ROOT_DIR = path.getabsolute(".")
+local BUILD_DIR = path.join(PROJ_DIR, "Projects")
+local THIRD_PARTY_DIR = path.join(ROOT_DIR, "ThirdParty")
 
-workspace "NeonShooter"
+local PROJECT_PREFIX = "NeonShooter"
+
+-- Load MaiLib
+local MAIDLIB_DIR = path.join(THIRD_PARTY_DIR, "MaiLib")
+local MaiLib = dofile(path.join(MAIDLIB_DIR, "premake5.mailib.lua"))
+
+workspace (PROJECT_PREFIX)
 do
     language "C++"
-    location (PROJ_BUILD_DIR)
+    location (path.join(BUILD_DIR, string.upper(_ACTION)))
 
     platforms { "x32", "x64" }
     configurations { "Debug", "Release" }
 
     configuration {}
 
-    rtti "Off"
-    exceptionhandling "Off"
+    MaiLib.cflags()
     
-    startproject "NeonShooter"
+    startproject (PROJECT_PREFIX)
 
     filter {}
 end
 
--- Load mojo project
-dofile (path.join(PROJ_DIR, "ThirdParty/Mojo/mojo.premake5.lua"))
-
 -- Game project
-project "NeonShooter"
+project (PROJECT_PREFIX)
 do
     kind "ConsoleApp"
-
-    links {
-        "Mojo"
-    }
 
     includedirs {
         path.join(PROJ_DIR, "ThirdParty/Mojo/include"),
@@ -53,6 +52,12 @@ do
             "/SAFESEH:NO"
         }
     end
+
+    filter {}
+
+    MaiLib.links(MAIDLIB_DIR)
+    MaiLib.files(MAIDLIB_DIR)
+    MaiLib.includedirs(MAIDLIB_DIR)
 
     filter {}
 end
